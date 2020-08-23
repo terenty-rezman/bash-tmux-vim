da31366a7f3ea447a561a8d13a058bbc48b0198f
terenty.rezman <terenty.rezman@mail.ru>
2020-08-23 18:07:51 +0000

    added vundle plugins autoinstall to from install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo -e "\ninstalling config files to $HOME...\n"
+
 # install vundle from git - vim package manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -12,3 +16,7 @@ cp -r $src/.vim/ $HOME/
 cp $src/.vimrc $HOME/
 
 ln -s -f .tmux/.tmux.conf $HOME/.tmux.conf
+
+vim +PluginInstall +qall
+
+echo -e "\ndone!\n"
