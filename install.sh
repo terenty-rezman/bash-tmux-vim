57edd13b3ff090fbdbef275e81e9b3da9c0fe840
terenty.rezman <terenty.rezman@mail.ru>
2020-08-25 21:26:42 +0000

    added double quotes for safety

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ set -e # terminate script on error
 function copy_file_if_exist {
   if [[ -f $1 ]] ; then # if file exists
     if [[ -L $1 ]] ; then # if a symlink
-      cp -P $1 $2
+      cp -P "$1" "$2"
     else
-      cp $1 $2
+      cp "$1" "$2"
     fi
   fi
 }
@@ -18,25 +18,25 @@ function copy_file_if_exist {
 function copy_dir_if_exist {
   if [[ -d $1 ]] ; then # if dir exists
     if [[ -L $1 ]] ; then # if a symlink
-      cp -P $1 $2
+      cp -P "$1" "$2"
     else
-      cp -R $1 $2
+      cp -R "$1" "$2"
     fi
   fi
 }
 
 function remove_file_if_exist {
   if [[ -f $1 ]] ; then # if file exists
-    rm -f $1
+    rm -f "$1"
   fi
 }
 
 function remove_dir_if_exist {
   if [[ -d $1 ]] ; then # if file exists
     if [[ -L $1 ]] ; then # if a symlink
-      rm -f $1
+      rm -f "$1"
     else
-      rm -r -f $1
+      rm -r -f "$1"
     fi
   fi  
 }
@@ -45,24 +45,24 @@ function backup_old_config {
   # check if backup dir does not exist yet -> if not -> backup files
   if [[ ! -d $backup_dir ]] ; then
     echo -e "old config stored in $backup_dir\n"
-    mkdir $backup_dir
+    mkdir "$backup_dir"
 
-    copy_file_if_exist $HOME/.bashrc $backup_dir/
-    copy_dir_if_exist $HOME/.tmux/ $backup_dir/
-    copy_file_if_exist $HOME/.tmux.conf.local $backup_dir/
-    copy_dir_if_exist $HOME/.vim/ $backup_dir/
-    copy_file_if_exist $HOME/.vimrc $backup_dir/
-    copy_file_if_exist $HOME/.tmux.conf $backup_dir/
+    copy_file_if_exist "$HOME/.bashrc" "$backup_dir/"
+    copy_dir_if_exist "$HOME/.tmux/" "$backup_dir/"
+    copy_file_if_exist "$HOME/.tmux.conf.local" "$backup_dir/"
+    copy_dir_if_exist "$HOME/.vim/" "$backup_dir/"
+    copy_file_if_exist "$HOME/.vimrc" "$backup_dir/"
+    copy_file_if_exist "$HOME/.tmux.conf" "$backup_dir/"
   fi
 }
 
 function remove_old_config {
-  remove_file_if_exist $HOME/.bashrc
-  remove_dir_if_exist $HOME/.tmux
-  remove_file_if_exist $HOME/.tmux.conf.local 
-  remove_dir_if_exist $HOME/.vim
-  remove_file_if_exist $HOME/.vimrc
-  remove_file_if_exist $HOME/.tmux.conf
+  remove_file_if_exist "$HOME/.bashrc"
+  remove_dir_if_exist "$HOME/.tmux"
+  remove_file_if_exist "$HOME/.tmux.conf.local"
+  remove_dir_if_exist "$HOME/.vim"
+  remove_file_if_exist "$HOME/.vimrc"
+  remove_file_if_exist "$HOME/.tmux.conf"
 }
 
 
@@ -70,12 +70,12 @@ function install_new_config {
   # install vundle from git - vim package manager
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-  cp $src/.bashrc $HOME/
-  cp -r $src/.tmux/ $HOME/
-  cp $src/.tmux.conf.local $HOME/
-  cp -r $src/.vim/ $HOME/
-  cp $src/.vimrc $HOME/
-  ln -s -f .tmux/.tmux.conf $HOME/.tmux.conf
+  cp "$src/.bashrc" "$HOME/"
+  cp -r "$src/.tmux/" "$HOME/"
+  cp "$src/.tmux.conf.local" "$HOME/"
+  cp -r "$src/.vim/" "$HOME/"
+  cp "$src/.vimrc" "$HOME/"
+  ln -s -f .tmux/.tmux.conf "$HOME/.tmux.conf"
 
   # make vim install plugins
   vim +PluginInstall +qall
